9498168cfb13cdc21ceddc4a948330e035f7f6a2
je-wong <jenern.wong@gmail.com>
2023-12-05 13:59:56 +1300

    enhanced test

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,25 +1,33 @@
 import { test, expect } from '@playwright/test';
 
+const testUserData = {
+  username: 'Test user',
+  password: '123456',
+};
+
 test('Login Test', async ({ page }) => {
-  // Navigate to the login page
+  // Step 1: Navigate to the login page
   await page.goto('https://demo.applitools.com/');
 
-  // Fill in the login form
-  await page.fill('#username', 'Test user');
-  await page.fill('#password', '123456');
+  // Step 2: Fill in the login form
+  await fillLoginForm(page, testUserData);
 
-  // Click the Sign in button
+  // Step 3: Click the Sign in button
   await page.click('#log-in');
 
-  // Wait for navigation or some element on the next page to appear
+  // Step 4: Wait for navigation or some element on the next page to appear
   await page.waitForSelector('#time');
 
-  // Verify that the login was successful
+  // Step 5: Verify that the login was successful
   const url = page.url();
-  expect(url).toContain('/app.html');;
-
-  // You can add more assertions as needed
+  expect(url).toContain('/app.html');
 
   // Take a screenshot for visual verification if needed
   await page.screenshot({ path: 'login-success.png' });
 });
+
+async function fillLoginForm(page, userData) {
+  await page.fill('#username', userData.username);
+  await page.fill('#password', userData.password);
+}
+
